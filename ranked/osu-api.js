cbe3a5ca36924e525c021c0db86d6fd5ee8346fc
MrLucky974 <HeroCreepyFR@gmail.com>
2020-10-04 13:23:50 +0400

    Deleted files

--- ranked/osu-api.js
+++
@@ -1,7 +0,0 @@
-const osuAPI = require('osuapi-js');
-const api = new osuAPI(APIKey, { beatmaps: { fetchCreatorInfo: false } });
-
-osuAPI.getUser('XeKr')
-    .then(user => {
-        console.log(user);
-    });
