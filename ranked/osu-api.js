51ebf2dd47aed83d6c9cade940649f20df7eb46d
MrLucky974 <HeroCreepyFR@gmail.com>
2020-10-04 13:16:14 +0400

    Added osuapi-js

--- ranked/osu-api.js
+++ ranked/osu-api.js
@@ -1,17 +1,7 @@
-fetch("https://osu.ppy.sh/oauth/token", {
-    method: 'post',
-    mode: 'no-cors',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-        "grant_type": "authorization_code",
-        "client_id": 2914,
-        "client_secret": "DuzliUBOmqBJ3NH6ljZLTCatUFN41guAlrnBvJ6h",
-        "redirect_uri": "https://notarealaddress.local/oauth/osu",
-        "code": "code"
-    })
-}).then(response => {
-    return response.json();
-});
+const osuAPI = require('osuapi-js');
+const api = new osuAPI(APIKey, { beatmaps: { fetchCreatorInfo: false } });
+
+osuAPI.getUser('XeKr')
+    .then(user => {
+        console.log(user);
+    });
