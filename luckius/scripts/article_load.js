02c1b804ed237a13e654605c6d389d406cb69862
MrLucky974 <HeroCreepyFR@gmail.com>
2021-03-23 22:31:34 +0400

    Moved JS file

--- luckius/scripts/article_load.js
+++ luckius/scripts/article_load.js
@@ -15,13 +15,3 @@ function getHash()
     
     }
 }
-
-function readJSONFile(filename)
-{
-    fs.readFile(filename, (err, raw) => {
-        if (err) throw err;
-        let data = JSON.parse(raw);
-        //console.log(student);
-        return data;
-    });
-}
