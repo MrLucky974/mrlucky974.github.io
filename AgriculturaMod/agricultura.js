9df03a9ed0b127d1985f4be633cb9fbd017ecf2d
MrLucky974 <HeroCreepyFR@gmail.com>
2021-09-20 18:01:02 +0400

    Update agricultura.js

--- AgriculturaMod/agricultura.js
+++ AgriculturaMod/agricultura.js
@@ -9,7 +9,14 @@ G.AddData({
 
     func:function() {
 
-        
+        new G.Res({
+            name:'wheat',
+            desc:'[wheat] is good',
+            icon:[],
+            turnToByContext:{'eat':{'health':0.01,'happiness':0.03},'decay':{'spoiled food':0.5}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
+            partOf:'food',
+            category:'food',
+        });
 
     }
 
