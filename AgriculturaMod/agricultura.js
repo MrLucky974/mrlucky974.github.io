c1dd5e827e5b48bbd2eefe205ef9f2571277b6d5
MrLucky974 <HeroCreepyFR@gmail.com>
2021-09-20 21:39:21 +0400

    Update agricultura.js

--- AgriculturaMod/agricultura.js
+++ AgriculturaMod/agricultura.js
@@ -31,10 +31,27 @@ G.AddData({
             category:'agriculture',
         });
 
+        new G.Res({
+            name:'parsnip',
+            desc:'[parsnip] can be found out in the wild and grown on farms.',
+            icon:[],
+            turnToByContext:{'eat':{'health':0.01,'happiness':0.03},'decay':{'spoiled food':0.5}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
+            partOf:'food',
+            category:'agriculture',
+        });
+
         //Seeds are used in farmfields to produce various vegetables, fruits and other plants
         new G.Res({
             name:'seed',
-            desc:'[seed] can be found on grass, maybe we can do something with that...',
+            desc:'[seed]s can be found on grass, maybe we can do something with that...',
+            icon:[],
+            turnToByContext:{},
+            category:'agriculture',
+        });
+
+        new G.Res({
+            name:'root',
+            desc:'[root]s are found in the wild, you can grow a lot of vegetables.',
             icon:[],
             turnToByContext:{},
             category:'agriculture',
@@ -50,19 +67,19 @@ G.AddData({
             desc: '@plants [seed]s and give all sorts of food',
             icon: [],
             cost:{'archaic building materials':50},
-            use:{'land': 1},
+            use:{'worker':1, 'land': 1},
 		    //staff:{'knapped tools':1},
 		    upkeep:{'water':1},
             gizmos: true,
             modes: {
                 'off': G.MODE_OFF,
-                //'any':{name:'Any',icon:[], desc:'Farm using any random [seed] you find. You may not get optimal results.',use:{'worker':1, 'knapped tools':1, 'seed':10}},
-                'wheat':{name:'Farm Wheat', icon:[], desc:'Produce [wheat] using [seed]s.', use:{'worker':1, 'knapped tools':1, 'seed':10}},
+                'any':{name:'Any',icon:[], desc:'Farm using any random [seed] or [root] you have. You may not get optimal results.', use:{'knapped tools':1, 'seed':10, 'root':10}},
+                'wheat':{name:'Farm Wheat', icon:[], desc:'Produce [wheat] using [seed]s.', use:{'knapped tools':1, 'seed':10}},
             },
             effects: [
                 //{type:'gather', context:'gather', what:{'herb':5}, max:30, notMode:'off'},
-                //{type:'gather', context:'gather', amount:10, max:30, mode:'any'},
-                {type:'gather', context:'farming', what:{'wheat':1}, amount:10, max:30, mode:'wheat'},
+                {type:'gather', context:'farming', what:{'wheat':0.1, 'parsnip':0.1}, amount:1, max:1, mode:'any'},
+                {type:'gather', context:'farming', what:{'wheat':1}, amount:1, max:1, mode:'wheat'},
             ],
             req: {/*'agriculture': true*/},
             category: 'production'
@@ -70,14 +87,14 @@ G.AddData({
 
         /* Techs */
 
-        /*new G.Tech({
+        new G.Tech({
             name:'agriculture',
             desc:'@unlocks [farmfield]s<>',
             icon:[],
             cost:{'insight':15},
             req:{'building':true},
             effects:[],
-        });*/
+        });
 
     }
 
