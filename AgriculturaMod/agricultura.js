25f757e228f9daa9c12b54385a90b3653f76e0d9
MrLucky974 <HeroCreepyFR@gmail.com>
2021-09-20 18:07:58 +0400

    Update agricultura.js

--- AgriculturaMod/agricultura.js
+++ AgriculturaMod/agricultura.js
@@ -9,14 +9,31 @@ G.AddData({
 
     func:function() {
 
+        G.resCategories['agriculture'] = {
+            name: 'Agriculture',
+            base: [],
+            side: []
+        };
+
         new G.Res({
             name:'wheat',
             desc:'[wheat] is good',
             icon:[],
             turnToByContext:{'eat':{'health':0.01,'happiness':0.03},'decay':{'spoiled food':0.5}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
             partOf:'food',
-            category:'food',
+            category:'agriculture',
         });
+
+        new G.Res({
+            name:'seeds',
+            desc:'[seeds] can be found on grass, maybe we can do something with that...',
+            icon:[],
+            turnToByContext:{},
+            partOf:'food',
+            category:'agriculture',
+        });
+
+        G.getDict('grass').res['gather']['seeds']=5;
 
     }
 
