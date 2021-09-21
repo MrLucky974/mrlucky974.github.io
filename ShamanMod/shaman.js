5bcad7801e6080253ff8ffeeb26696bc1bf299fb
MrLucky974 <HeroCreepyFR@gmail.com>
2021-09-21 14:44:37 +0400

    Update shaman.js

--- ShamanMod/shaman.js
+++ ShamanMod/shaman.js
@@ -11,13 +11,37 @@ G.AddData({
 
     func:function() {
 
-        //New modes for the healer
-        G.getDict('healer')['gizmos'] = true;
-        G.getDict('healer').modes['heal']={name:'Heal', desc:'The [healer] heals the [sick] and [wounded].', req:{}};
-        G.getDict('healer').effects[0]['mode'] = 'heal';
-        G.getDict('healer').effects[1]['mode'] = 'heal';
-        
-        //G.getDict('healer').effects.push({type:'convert',from:{'flour':3, 'water':2}, into:{'dough':4}, every:3, mode:'dough'});
+        new G.Res({
+            name:'medicine',
+            desc:'[medicine] helps to heal the [sick] and the [wounded].',
+            icon:[],
+            //turnToByContext:{'decay':{'medicine':0.3,}},
+            //partOf:'food',
+            category:'food',
+        });
+
+        new G.Unit({
+            name:'shaman',
+            desc:'@uses [medicine]s to heal the [sick] and the [wounded] with a greater success rate than the [healer]<>Produce [medicine] by combining [herb]s secret powers and the magic of spirits.<>The [shaman] knows the secrets of special plants that make illness stay away by transforming them.',
+            icon:[23,3],
+            cost:{},
+            use:{'worker':1},
+            staff:{'knapped tools':1},
+            upkeep:{'coin':0.2},
+            gizmos: true,
+            modes: {
+                'heal':{name:'Heal', icon:[], desc:''},
+                'medicine':{name:'Make medicine', icon:[3,5], desc:''},
+            },
+            effects:[
+                {type:'convert', from:{'sick':1,'medicine':2.5, 'spirituality':1}, into:{'adult':1}, chance:1/2, every:1.5, mode:'heal'},
+                {type:'convert', from:{'wounded':1, 'medicine':2.5, 'spirituality':1}, into:{'adult':1}, chance:1/3, every:5, mode:'heal'},
+                {type:'convert', from:{'water':3, 'herb':10}, into:{'medicine':7}, every:3, mode:'medicine'},
+            ],
+            req:{'healing':true, 'ritualism':true},
+            category:'spiritual',
+            priority:5,
+        });
 
     }
 
