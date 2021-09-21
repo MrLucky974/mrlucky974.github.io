7279584b62314fadbcc9620f34f7f120262e858a
MrLucky974 <HeroCreepyFR@gmail.com>
2021-09-21 08:42:22 +0400

    Update agricultura.js
    - Individual crop types replaced by generic ressources (cerals & vegetables)
    - modified some values
    - moved some categories

--- AgriculturaMod/agricultura.js
+++ AgriculturaMod/agricultura.js
@@ -17,33 +17,33 @@ G.AddData({
         G.resCategories['agriculture'] = {
             name: 'Agriculture',
             base: [],
-            side: []
+            side: ['seed', 'root']
         };
 
         /* Ressources */
         
         new G.Res({
-            name:'wheat',
-            desc:'[wheat] is a product of a long and natural process, you can use it to produce food.',
+            name:'vegetable',
+            desc:'[vegetable, Vegetables]',
             icon:[],
-            turnToByContext:{'eat':{'health':0.01,'happiness':0.03},'decay':{'spoiled food':0.5}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
+            turnToByContext:{'eat':{'health':0.02,'happiness':0.02},'decay':{'spoiled food':1}},
             partOf:'food',
-            category:'agriculture',
+            category:'food',
         });
 
         new G.Res({
-            name:'parsnip',
-            desc:'[parsnip] can be found out in the wild and grown on farms.',
+            name:'cereal',
+            desc:'[cereal, Cereals]',
             icon:[],
-            turnToByContext:{'eat':{'health':0.01,'happiness':0.03},'decay':{'spoiled food':0.5}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
+            turnToByContext:{'eat':{'health':0.005,'happiness':-0.03},'decay':{'spoiled food':0.3}},
             partOf:'food',
-            category:'agriculture',
+            category:'food',
         });
 
         //Seeds are used in farmfields to produce various fruits and cereals
         new G.Res({
             name:'seed',
-            desc:'[seed]s can be found on grass, maybe we can do something with that...',
+            desc:'[seed]s grow fruits and cereals.',
             icon:[],
             turnToByContext:{'eating':{'health':0.005,'happiness':-0.03}},
             category:'agriculture',
@@ -51,38 +51,50 @@ G.AddData({
 
         new G.Res({
             name:'root',
-            desc:'[root]s are found in the wild, you can grow a lot of vegetables.',
+            desc:'[root]s are found in the wild, you can grow a lot of different vegetables.',
             icon:[],
-            turnToByContext:{'eating':{'health':0.005,'happiness':-0.03}},
+            turnToByContext:{'eating':{'health':0.005,'happiness':-0.005}},
             category:'agriculture',
         });
 
+        /* Goods */
+
         G.getDict('grass').res['gather']['seed']=0.01;
         G.getDict('grass').res['gather']['root']=0.015;
-
+        
         G.contextNames['farming']='Farming';
+
+        new G.Goods({
+            name:'wild vegetables',
+            desc:'[wild vegetables, Wild vegetables] are a good source of [root]s;',
+            icon:[],
+            res:{
+                'gather':{'root':5},
+            },
+            mult:10,
+        });
 
         /* Units */
         
         new G.Unit({
-            name: 'farmfield',
-            desc: '@plants [seed]s and give all sorts of food',
+            name: 'farmland',
+            desc: '@',
             icon: [],
-            cost:{'archaic building materials':50},
+            cost:{'archaic building materials':0},
             use:{'worker':1, 'land': 1},
-		    upkeep:{'water':0.1},
+		    upkeep:{'water':0.35},
             gizmos: true,
             modes: {
                 'off': G.MODE_OFF,
-                'any':{name:'Any', icon:[], desc:'Farm using any random [seed] or [root] you have. You may not get optimal results.', use:{'knapped tools':1, 'seed':5, 'root':5}},
-                'wheat':{name:'Farm wheat', icon:[], desc:'Produce [wheat] using [seed]s.', use:{'knapped tools':1, 'seed':10}},
-                'parsnip':{name:'Farm wheat', icon:[], desc:'Produce [parsnip] using [root]s.', use:{'knapped tools':1, 'root':10}},
+                'any':{name:'Any', icon:[], desc:'Farm using any random [seed] or [root] you have. You may not get optimal results.', use:{'knapped tools':1}},
+                'vegetables':{name:'Farm vegetables', icon:[], desc:'Produce [vegetable]s using [root]s.', use:{'knapped tools':1}, upkeep:{'root':10}},
+                'cereals':{name:'Farm cereals', icon:[], desc:'Produce [cereal]s using [seed]s.', use:{'knapped tools':1}, upkeep:{'seed':10}},
             },
             effects: [
-                //{type:'gather', context:'gather', what:{'herb':5}, max:30, notMode:'off'},
+                {type:'gather', context:'gather', what:{'herb':50}, amount:5, max:30, mode:'off'},
                 {type:'gather', context:'farming', what:{'wheat':0.1, 'parsnip':0.2}, amount:1, max:3, mode:'any'},
-                {type:'gather', context:'farming', what:{'wheat':1}, amount:1, max:5, mode:'wheat'},
-                {type:'gather', context:'farming', what:{'parsnip':1}, amount:1, max:5, mode:'parsnip'},
+                {type:'gather', context:'farming', what:{'wheat':1}, amount:7, max:10, mode:'cereals'},
+                {type:'gather', context:'farming', what:{'parsnip':1}, amount:7, max:10, mode:'vegetables'},
                 {type:'mult', value:2.0, req:{'harvest rituals':'on'}}
             ],
             req: {/*'agriculture': true*/},
