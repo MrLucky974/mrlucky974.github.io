8897d9d784503c22e573a6198457cf2e10d61e88
MrLucky974 <HeroCreepyFR@gmail.com>
2021-09-21 15:45:23 +0400

    Update shaman.js
    - Added new workforce type : zombies
    - Changed necromancy interaction of shaman

--- ShamanMod/shaman.js
+++ ShamanMod/shaman.js
@@ -33,14 +33,14 @@ G.AddData({
                 'heal':{name:'Heal', icon:[], desc:'Heal the [sick] and the [wounded] with [medicine] and 1 [insight].'},
                 'medicine':{name:'Make medicine', icon:[3,5], desc:'Produce 7 [medicine] from 3 [water] and 10 [herb]s.'},
                 'youth':{name:'Cure elders', icon:[4,3], desc:'The [shaman] have a very small chance to cure [elder]s, them becoming [adult]s again.<>Use 20 [medicine], 1 [insight] & 3 [faith].'},
-                'necromancy':{name:'Ressurect corpses', icon:[14,1], desc:'Using [necromancy], a [shaman] can ressurect [corpse, dead] people back to life.<>Use 5 [faith].', req:{'necromancy':true}},
+                'necromancy':{name:'Ressurect corpse', icon:[14,1], desc:'Using [necromancy], a [shaman] can ressurect [corpse, dead] people back to life, making a [zombie].<>Use 5 [faith].', req:{'necromancy':true}},
             },
             effects:[
                 {type:'convert', from:{'sick':1, 'medicine':7, 'insight':1}, into:{'adult':1}, chance:1/2, every:1.5, mode:'heal'},
                 {type:'convert', from:{'wounded':1, 'medicine':7, 'insight':1}, into:{'adult':1}, chance:1/3, every:5, mode:'heal'},
                 {type:'convert', from:{'water':3, 'herb':10}, into:{'medicine':7}, every:3, mode:'medicine'},
                 {type:'convert', from:{'elder':1, 'medicine':20, 'insight':1, 'faith':3}, into:{'adult':1}, chance:1/7, every:10, mode:'youth'},
-                {type:'convert', from:{'corpse':1, 'faith':5}, into:{'adult':1}, chance:1/10, every:10, mode:'necromancy'},
+                {type:'convert', from:{'corpse':1, 'faith':5}, into:{'zombie':1}, chance:1/10, every:10, mode:'necromancy'},
             ],
             req:{'healing':true, 'ritualism':true},
             category:'spiritual',
@@ -49,13 +49,23 @@ G.AddData({
 
         new G.Tech({
             name:'necromancy',
-            desc:'@unlocks a new option for [shaman]s<>',
+            desc:'@unlocks new options for [shaman]s<>',
             icon:[14,1],
             cost:{'insight':10, 'faith':1},
             req:{'ritualism':true, 'burial':true, 'healing':true},
             effects:[
             ],
             chance:2,
+        });
+
+        new G.Res({
+            name:'zombie',
+            desc:'[zombie,Zombies] are [corpse]s bringed back to life by a [shaman] using [necromancy].//Zombies make are an addition to your [worker,workforce].',
+            //startWith:5,
+            visible:true,
+            partOf:'worker',
+            category:'demog',
+            icon:[4,3],
         });
 
     }
