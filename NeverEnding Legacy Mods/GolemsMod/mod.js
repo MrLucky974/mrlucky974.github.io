114d74ce2858ad7caa3465fc6e56ebd40ce6d55a
MrLucky974 <HeroCreepyFR@gmail.com>
2021-09-23 11:06:07 +0400

    Update mod.js

--- NeverEnding Legacy Mods/GolemsMod/mod.js
+++ NeverEnding Legacy Mods/GolemsMod/mod.js
@@ -19,12 +19,6 @@ G.AddData({
 
     func:function() {
 
-        G.resCategories['golems'] = {
-            name: 'Golems',
-            base: [],
-            side: []
-        };
-
         M = {}; //Short term for mod
 
         M.golems = []
@@ -69,21 +63,37 @@ G.AddData({
                     });
                 };
             }
-            
-            //if (currentTick%250==0) console.log('mod update');
 
             currentTick++;
         }
 
         setInterval(M.update, 1); //Loop update every 1 ms
 
+        //Golems category
+        G.resCategories['golems'] = {
+            name: 'Golems',
+            base: ['mud golem', 'clay golem'],
+            side: ['golems']
+        };
+
+        //Golems group
         new G.Res({
-            name:'mud golem',
-            desc:'[mud golem, Golems] made from mud.//[mud golem, Golems] make are an addition to your [worker,workforce].//[mud golem, Golems] dies after a certain amount of time.',
+            name:'golems',
+            desc:'[golems] are creatures made of inorganic materials in which life has been deposited.//They can be made of [clay] or [mud] by an [artisan].',
             //startWith:5,
             visible:true,
             partOf:'worker',
             category:'golems',
+        });
+
+        //Mud golem ressource
+        new G.Res({
+            name:'mud golem',
+            desc:'[mud golem, Golems] made from [mud].//[mud golem, Golems] make are an addition to your [worker,workforce].//[mud golem, Golems] dies after a certain amount of time.',
+            //startWith:5,
+            visible:true,
+            partOf:'golems',
+            //category:'golems',
             icon:[0,0],
             tick:function(me,tick)
 		    {  
@@ -120,33 +130,33 @@ G.AddData({
             }
         });
 
-        //Clay golem
+        //Clay golem ressource
         new G.Res({
             name:'clay golem',
-            desc:'[clay golem, Golems] made from clay.//[clay golem, Golems] make are an addition to your [worker,workforce].//[clay golem, Golems] dies after a certain amount of time.',
+            desc:'[clay golem, Golems] made from [clay].//[clay golem, Golems] make are an addition to your [worker,workforce].//[clay golem, Golems] dies after a certain amount of time.',
             //startWith:5,
             visible:true,
-            partOf:'worker',
-            category:'golems',
+            partOf:'golems',
+            //category:'golems',
             icon:[0,0],
             tick:function(me,tick)
 		    {  
                 if (me.amount>0)
 			    {
-                    if (tick%1==0) 
+                    if (tick%1==0) //Every second
                     {
                         var toRemove = [];
 
                         for (let golemId = 0; golemId < M.golems.length; golemId++) {
                             const golem = M.golems[golemId];
 
-                            if (golem != undefined)
+                            if (golem != undefined) //Counter an error
                             {
-                                if (golem.type == 'clay')
+                                if (golem.type == 'clay') //Check that the current golem is compatible
                                 {
                                     golem.lifetime+=Math.random() * 2;
 
-                                    if (golem.lifetime >= golem.maxLife) 
+                                    if (golem.lifetime >= golem.maxLife) //Golem lived their life
                                     {
                                         G.lose(me.name, 1, 'golem death');
                                         toRemove.push(golemId);
@@ -156,7 +166,7 @@ G.AddData({
                         }
 
                         for (let i = 0; i < toRemove.length; i++) {
-                            const id = toRemove[i];
+                            const id = toRemove[i]; //Remove the data from the list
                             delete M.golems[id];
                         }
                     }
